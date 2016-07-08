cc7b2222ee7c24e232011f886c0cd2a456243861
wxmaudio <wxm224@163.com>
2016-07-08 11:40:32 +0800

    Add loaction search

--- SearchPage.js
+++ SearchPage.js
@@ -127,6 +127,22 @@ class SearchPage extends React.Component{
 		this._executeQuery(query);
 	}
 	 
+	onLocationPressed(){
+		console.log("Start get position.");
+		navigator.geolocation.getCurrentPosition(
+			location=>{
+				console.log(location);
+				var search = location.coords.latitude+','+location.coords.longitude;
+				this.setState({searchString:search});
+				var query = urlForQueryAndPage('centre_point', search, 1);
+				this._executeQuery(query);
+			},
+			error=>{
+				this.setState({message:'There was a problem with obtaining your location: '+ error.message});
+			}
+		)
+	}
+
 	render(){
 		var spinner = this.state.isLoading?(<ActivityIndicator size='large'/>):(<View/>);
 
@@ -142,7 +158,7 @@ class SearchPage extends React.Component{
 				    </TouchableHighlight>
 			    </View>
 			    <TouchableHighlight style={styles.button}
-			    underlayColor='#99d9f4'>
+			    underlayColor='#99d9f4' onPress={this.onLocationPressed.bind(this)}>
 			    <Text style={styles.buttonText}>Location</Text>
 			    </TouchableHighlight>
 			    <Image source={require('./Resources/house.png')} style={styles.image}/>
